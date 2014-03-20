751a4616c843f404ad000f5d72f542e438ba25da
Jonathan Azoff <jon@azoffdesign.com>
2014-03-19 22:04:29 -0700

    Initial Commit

---
+++ yelpclient/AZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AZAppDelegate.h
+//  yelpclient
+//
+//  Created by Jonathan Azoff on 3/19/14.
+//  Copyright (c) 2014 Jonathan Azoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ yelpclient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yelpclient
+//
+//  Created by Jonathan Azoff on 3/19/14.
+//  Copyright (c) 2014 Jonathan Azoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AZAppDelegate class]));
+    }
+}
